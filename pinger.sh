97289c5f7b21c1a85bc5d2b8d8af3125e2bc73fc
root <root@curie.nt.ntnu.no>
2015-04-17 12:41:19 +0200

    new solution

--- pinger.sh
+++ pinger.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# Generating IP addresses
-python /usr/local/share/pinger/adrgen.py
+sqlite3 /usr/local/share/innloggingslogging/innlogging.db "select ip from  maskiner" | fping -c1 -t300 -u 2>&1 | awk '{
+ if ( $5 == "1/1/0%," ) {
+ print "update maskiner set pingerstatus = \x27Ok\x27 where ip = \x27" $1 "\x27" ";"  
+}
+ else if ( $5 == "1/0/100%" ) {
+ print "update maskiner set pingerstatus = \x27Av/timeout\x27 where ip = \x27" $1 "\x27" ";" 
+}
+ else if ( $2 == "address" ) {
+ print "update maskiner set pingerstatus = \x27-\x27 where ip = \x27" $1 "\x27" ";"  
+} 
+}' | sudo sqlite3 /usr/local/share/innloggingslogging/innlogging.db
 
-# Running the ping command
-fping -c1 -t300 -u < /usr/local/share/pinger/adr \
-			> /usr/local/share/pinger/output 2>&1
-
-# Generating the html page
-python /usr/local/share/pinger/htmlgen.py
